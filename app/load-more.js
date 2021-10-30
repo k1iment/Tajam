07410ed2e6a9a36726c7c2bf7311c96a4b50defd
Карташова Маргарита <morgana.net@yandex.ru>
2021-10-30 12:33:19 +0300

    modified

--- app/load-more.js
+++ app/load-more.js
@@ -14,7 +14,32 @@ document.addEventListener('DOMContentLoaded', function() {
             let finalNumber = parseInt(number);
             
             if (finalNumber===integer) {
-                i.classList.remove('none')
+                i.classList.remove('none-1')
+            }
+            if (integer>2) {
+                return integer = 0;
+            }
+        })
+
+        document.querySelector("#hide-btn").classList.remove("none")
+    })
+
+    let hide_btn = document.querySelector("#hide-btn");
+
+    let second_integer = 0;
+
+    hide_btn.addEventListener('click', function(){
+        second_integer+=1;
+        
+        let sheet = document.querySelectorAll('.project-pictures-container');
+
+        sheet.forEach(function(i){
+            let number = i.getAttribute("data-path")
+            
+            let finalNumber = parseInt(number);
+            
+            if (finalNumber != 3) {
+                i.classList.add('none-1')
             }
         })
     })
